a6b8daedcec18cff8bfa2bca16bb1e01b6112ebf
Alesia Yarmash <120108084+lesiayarm@users.noreply.github.com>
2025-04-24 18:27:53 +0300

    Create 0.6. Depth-first search.cpp

---
+++ 0.6. Depth-first search.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+
+int counter = 1;
+
+void bfs(std::vector<std::vector<int>>& matrix, std::vector<int>& tags, int u, int n) {
+    if (tags[u] != -1) {
+        return;
+    }
+    tags[u] = counter;
+    ++counter;
+    int j = 0;
+    while (j < n) {
+        if (matrix[u][j] != 1) {
+            ++j;
+            continue;
+        }
+        if (matrix[u][j] == 1) {
+            matrix[u][j] = 0;
+            bfs(matrix, tags, j, n);
+        }
+        ++j;
+    }
+}
+
+int main() {
+    std::ifstream cin("input.txt");
+    std::ofstream cout("output.txt");
+    int n;
+    cin >> n;
+    std::vector<std::vector<int>> matrix(n, std::vector<int>(n));
+    std::vector<int> tags(n, -1);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (tags[i] == -1) {
+            bfs(matrix, tags, i, n);
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        cout << tags[i] << " ";
+    }
+    return 0;
+}
